865fc2732108193095e029dcf7e7c39c4b4f8b81
Leo Niemi <50656714+olenleo@users.noreply.github.com>
2021-05-06 14:54:57 +0300

    Update matteprov.py

--- matteprov.py
+++ matteprov.py
@@ -1,27 +1,24 @@
 import random
 
-print("Multiplikation")
+print("Vad vill du öva på?")
 
-score = 0
-ratt_svar_i_rad = 0
-print("Vilken tabell vill du öva på?")
-siffra = int(input()) # int()
+raknesatt = input() 
+if raknesatt == "plus":
+    print("Inte färdigt")
 
-while score < 10:
-    annanSiffra = random.randint(0,10)
-    svar = siffra * annanSiffra
+   
+if raknesatt == "multiplikation":
+    score = 0
+    ratt_svar_i_rad = 0
+    print("Vilken tabell vill du öva på?")
+    siffra = int(input()) # int()
 
-    print(siffra, " * ", annanSiffra, " = ")
-    spelarens_svar = int(input())  # Vaddå int?
+    while score < 10:
+        annanSiffra = random.randint(0,10)
+        svar = siffra * annanSiffra
+
+        print(siffra, " * ", annanSiffra, " = ")
+        spelarens_svar = int(input()) 
 
     if svar == spelarens_svar:
         print("Rätt! DU ÄR BRA!")
-        score = score + 1
-        ratt_svar_i_rad = ratt_svar_i_rad + 1
-    else:
-        print("Fel!")
-        ratt_svar_i_rad = 0
-
-    print("Du har", score, "poäng")
-
-print("Provet är slut. Du fick ", ratt_svar_i_rad, " i rad.")
