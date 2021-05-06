4fd6121000a599c3e337d02ca01e69160eb8bd9b
Leo Niemi <50656714+olenleo@users.noreply.github.com>
2021-05-06 15:29:19 +0300

    Update matteprov.py

--- matteprov.py
+++ matteprov.py
@@ -2,27 +2,28 @@ import random
 
 print("Vad vill du öva på?")
 
-raknesatt = input() 
+raknesatt = input()
+
 if raknesatt == "plus":
     score = 0
     ratt_svar_i_rad = 0
    
-    siffra = int(input()) # int()
-
     while score < 10:
-        annanSiffra = random.randint(0,10)
-        svar = siffra * annanSiffra
-
-        print(siffra, " * ", annanSiffra, " = ")
+        
+        siffra = random.randint(0,100)
+        annanSiffra = random.randint(0,100)
+        svar = siffra + annanSiffra
+        
+        print(siffra, " + ", annanSiffra, " = ")
         spelarens_svar = int(input()) 
 
-    if svar == spelarens_svar:
-        print("Rätt! DU ÄR BRA!")
-        score = score + 1
-        ratt_svar_i_rad = ratt_svar_i_rad + 1
-    else:
-        print("Fel!")
-        ratt_svar_i_rad = 0
+        if svar == spelarens_svar:
+            print("Rätt! DU ÄR BRA!")
+            score = score + 1
+            ratt_svar_i_rad = ratt_svar_i_rad + 1
+        else:
+            print("Fel!")
+            ratt_svar_i_rad = 0
 
     print("Du har", score, "poäng")
     
@@ -42,13 +43,13 @@ if raknesatt == "multiplikation":
         print(siffra, " * ", annanSiffra, " = ")
         spelarens_svar = int(input()) 
 
-    if svar == spelarens_svar:
-        print("Rätt! DU ÄR BRA!")
-        score = score + 1
-        ratt_svar_i_rad = ratt_svar_i_rad + 1
-    else:
-        print("Fel!")
-        ratt_svar_i_rad = 0
+        if svar == spelarens_svar:
+            print("Rätt! DU ÄR BRA!")
+            score = score + 1
+            ratt_svar_i_rad = ratt_svar_i_rad + 1
+        else:
+            print("Fel!")
+            ratt_svar_i_rad = 0
 
     print("Du har", score, "poäng")
     
